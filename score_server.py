60d9bc4544df177392c52e7185b38d8bae33b5bf
simchacohen1 <134088258+simchacohen1@users.noreply.github.com>
2026-08-09 00:25:21 -0400

    mnbm

--- score_server.py
+++ score_server.py
@@ -211,7 +211,7 @@ def score():
             # error in this terminal window.
             print(f"Transcription error: {e}")
             return jsonify({
-        	   "error": f"DEBUG: {e}"
+        	   "Could not process this recording. Please try recording again."
             }), 400
     finally:
         os.remove(tmp_path)
